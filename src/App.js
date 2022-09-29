84d10bcc655334a799c664ea1939acee1a8698da
taddjv <taddjv111@gmail.com>
2022-09-29 00:02:55 +0000

    Update package-lock.json and App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 import Home from './components/Home';
 import Stocks from './components/Stocks';
 import Movies from './components/Movies';
+
 
 function App() {
   return (
     <div className='main'>
       <h1>App Component</h1>
+      <nav className="comp nav">
+        <ul>
+          <li>
+            <a href="/movies">Anchor</a>
+          </li>
+          <li>
+            <Link>Home</Link>
+          </li>
+        </ul>
+        </nav>
       <Switch>
         <Route exact path='/'>
           <Home />
